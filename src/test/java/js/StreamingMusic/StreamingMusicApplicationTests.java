8a506c547da7450af35859e7d6b11e39b1e3b850
park <qkrwnstns52@naver.com>
2021-01-17 19:10:25 +0900

    initial commit

---
+++ src/test/java/js/StreamingMusic/StreamingMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package js.StreamingMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
